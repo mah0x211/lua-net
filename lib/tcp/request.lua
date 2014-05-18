8aaf742e13f8b75b9dd5c62aead7650089bf6dde
Masatoshi Teruya <mah0x211@gmail.com>
2014-05-18 13:54:00 +0900

    change: codebase overhauled and event methods has been added.

--- lib/tcp/request.lua
+++ lib/tcp/request.lua
@@ -28,13 +28,18 @@
 --]]
 
 local lls = require('llsocket');
-local buffer = require('buffer');
-local DEFAULT_BUFSIZE = 2048;
+local coevent = require('coevent');
+-- list of notifications
+local NOTIFICATION = {
+    ['close'] = true,
+    ['read'] = true,
+    ['write'] = true,
+    ['hup'] = true
+};
 
 -- metamethods
 local function GC()
     lls.close( self.fd );
-    self.buf:free();
 end
 
 
@@ -47,48 +52,153 @@ local MT = {
 
 
 -- method implementaion
-function METHOD:read()
-    return self.buf:read( self.fd );
-end
-
-
-function METHOD:write( buf, ... )
-    return buf:write( self.fd, ... );
-end
-
-
 function METHOD:shutdown()
     return lls.shutdown( self.fd, lls.opt.SHUT_RDWR );
 end
 
+function METHOD:shutdownRD()
+    self:unwatchRead();
+    return lls.shutdown( self.fd, lls.opt.SHUT_RD );
+end
+
+function METHOD:shutdownWR()
+    self:unwatchWrite();
+    return lls.shutdown( self.fd, lls.opt.SHUT_WR );
+end
+
 
 function METHOD:close()
-    self.buf:free();
-    return lls.close( self.fd );
+    local err;
+    
+    self:unwatch();
+    err = lls.close( self.fd );
+    self:notify( 'close', self, err );
+    
+    return err;
+end
+
+
+-- event methods
+local function onRead( self, watcher, hup )
+    -- notify hup event
+    if hup then
+        self:notify( 'hup', self );
+        self:unwatch();
+    -- notify read event
+    else
+        self:notify( 'read', self );
+    end
+end
+
+function METHOD:watchRead( oneshot, edgeTrigger )
+    return self.read_ev:watch( oneshot, edgeTrigger, onRead, self );
+end
+
+function METHOD:unwatchRead()
+    if self.read_ev then
+        return self.read_ev:unwatch();
+    end
+end
+
+
+local function onWrite( self, watcher, hup )
+    -- notify hup event
+    if hup then
+        self:notify( 'write hup', self );
+        self:unwatch();
+    -- notify write event
+    else
+        self:notify( 'write', self );
+    end
+end
+
+function METHOD:watchWrite( oneshot, edgeTrigger )
+    return self.write_ev:watch( oneshot, edgeTrigger, onWrite, self );
+end
+
+function METHOD:unwatchWrite()
+    if self.write_ev then
+        return self.write_ev:unwatch();
+    end
+end
+
+
+function METHOD:unwatch()
+    self:unwatchRead();
+    self:unwatchWrite();
+end
+
+
+function METHOD:observe( name, callback, ctx )
+    if type( name ) ~= 'string' then
+        error( 'name must be type of string' );
+    elseif not NOTIFICATION[name] then
+        error( 'unknown notification name: ' .. name );
+    elseif type( callback ) ~= 'function' then
+        error( 'callback must be type of function' );
+    else
+        self.obs[name] = {
+            fn = callback,
+            ctx = ctx
+        };
+    end
+    
+    return self;
+end
+
+
+function METHOD:unobserve( name )
+    if type( name ) ~= 'string' then
+        error( 'name must be type of string' );
+    elseif not NOTIFICATION[name] then
+        error( 'unknown notification name: ' .. name );
+    else
+        self.obs[name] = nil;
+    end
+    
+    return self;
+end
+
+
+function METHOD:notify( name, ... )
+    local obs = self.obs[name];
+    
+    if obs then
+        obs.fn( obs.ctx, ... );
+    end
 end
 
 
 -- interfaces
-local function accept( fd, nonblock, bufsize )
-    local fd, err = lls.accept( fd, nonblock );
+local function create( fd )
+    return setmetatable({
+        fd = fd,
+        obs = {}
+    }, MT );
+end
+
+
+local function createWithEventLoop( fd, loop )
+    local req = create( fd );
+    local err;
     
+    -- create read event
+    req.read_ev, err = coevent.reader( loop, fd );
+    -- create write event
     if not err then
-        local buf;
-        -- create buffer
-        buf, err = buffer( bufsize or DEFAULT_BUFSIZE );
-        if not err then
-            -- return instance
-            return setmetatable({
-                fd = fd,
-                buf = buf
-            }, MT );
-        end
+        req.write_ev, err = coevent.writer( loop, fd );
     end
     
-    return nil, err;
+    -- dispose
+    if err and req then
+        req:close();
+    end
+    
+    return req, err;
 end
 
 return {
-    accept = accept;
+    create = create,
+    createWithEventLoop = createWithEventLoop
 }
 
