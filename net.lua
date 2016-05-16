97c15a32a173a0df3f4edae3b190355293545e40
Masatoshi Teruya <mah0x211@gmail.com>
2016-05-16 11:17:39 +0900

    eliminate trailing whitespace

--- net.lua
+++ net.lua
@@ -1,17 +1,17 @@
 --[[
-  
+
   Copyright (C) 2014 Masatoshi Teruya
- 
+
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
   in the Software without restriction, including without limitation the rights
   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
   copies of the Software, and to permit persons to whom the Software is
   furnished to do so, subject to the following conditions:
- 
+
   The above copyright notice and this permission notice shall be included in
   all copies or substantial portions of the Software.
- 
+
   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
@@ -19,12 +19,12 @@
   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
   THE SOFTWARE.
-  
-  
+
+
   net.lua
   lua-net
   Created by Masatoshi Teruya on 14/05/16.
-  
+
 --]]
 
 -- constants
@@ -246,7 +246,7 @@ end
 
 --- recv
 -- @return  str
--- @return  err 
+-- @return  err
 -- @return  again
 function Socket:recv( bufsize )
     return self.sock:recv( bufsize );
@@ -282,7 +282,7 @@ end
 -- @return  err
 function Socket:sendy( str )
     local len, err, again = self:send( str );
-    
+
     -- no space of send buffer
     if again then
         yield();
