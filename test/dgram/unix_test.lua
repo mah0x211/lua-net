7fb1ca8149a835dcd98181bcfc392807f3de96a7
Masatoshi Teruya <mah0x211@gmail.com>
2021-11-15 14:42:23 +0900

    Add test/dgram/unix_test.lua

---
+++ test/dgram/unix_test.lua
@@ -0,0 +1,81 @@
+require('luacov')
+require('nosigpipe')
+local assert = require('assertex')
+local testcase = require('testcase')
+local net = require('net')
+local unix = require('net.dgram.unix')
+
+local PATHNAME
+function testcase.before_all()
+    PATHNAME = './' .. os.time() .. '.sock'
+end
+
+function testcase.after_each()
+    os.remove(PATHNAME)
+end
+
+function testcase.after_all()
+    os.remove(PATHNAME)
+end
+
+function testcase.new()
+    -- test that create new instance of net.dgram.unix.Socket
+    local s = assert(unix.new())
+    assert.match(tostring(s), '^net.dgram.unix.Socket: ', false)
+    assert(not s.nonblock, 'nonblocking mode')
+    assert.equal(s:family(), net.AF_UNIX)
+    assert.equal(s:socktype(), net.SOCK_DGRAM)
+    assert.equal(s:protocol(), 0)
+    s:close()
+end
+
+function testcase.bind()
+    local s = assert(unix.new())
+
+    -- test that bind to pathanme
+    local _, _, ai = assert(s:bind(PATHNAME))
+    assert.equal(ai:addr(), PATHNAME)
+    s:close()
+
+    -- test that returns an error that name too long
+    local _, err = s:bind('./long-name-' .. string.rep('0', 500) .. '.sock')
+    assert.match(err, 'too long')
+
+    -- test that returns an error that already in use
+    local s2 = assert(unix.new())
+    _, err = s2:bind(PATHNAME)
+    assert.match(err, ' already ')
+    s2:close()
+end
+
+function testcase.connect()
+    local s = assert(unix.new())
+    assert(s:bind(PATHNAME))
+
+    -- test that connect to pathanme
+    local c = assert(unix.new())
+    local _, _, _, cai = assert(c:connect(PATHNAME))
+    assert.equal(cai:addr(), PATHNAME)
+    c:close()
+    s:close()
+
+    -- test that returns an error that name too long
+    local _, err = c:connect('./long-name-' .. string.rep('0', 500) .. '.sock')
+    assert.match(err, 'too long')
+
+    -- test that returns an error that already in use
+    c = assert(unix.new())
+    _, err = c:connect(PATHNAME)
+    assert.match(err, 'refused')
+    c:close()
+end
+
+function testcase.pair()
+    -- test that create new pair instance of net.dgram.unix.Socket
+    local sp = assert(unix.pair(PATHNAME))
+    assert.equal(#sp, 2)
+    for _, s in ipairs(sp) do
+        assert.match(tostring(s), '^net.dgram.unix.Socket: ', false)
+        s:close()
+    end
+end
